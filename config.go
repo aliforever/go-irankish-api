f07a2d514387588e2e9e63b9996cb1282b1c0ba7
aliforever <alipadida@live.com>
2023-01-07 22:35:39 +0300

    fixed redirecturl

--- config.go
+++ config.go
@@ -5,7 +5,7 @@ import "net/url"
 const (
 	IranKishShaparakUrl = "https://ikc.shaparak.ir"
 	TokenUrl            = "/api/v3/tokenization/make"
-	RedirectUrl         = "/iuiv3/IPG/Index/"
+	RedirectUrl         = IranKishShaparakUrl + "/iuiv3/IPG/Index/"
 	ConfirmationUrl     = "/api/v3/confirmation/purchase"
 )
 
