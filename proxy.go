c8398e2a52cc9cbebbc27994cc285fae4a6f5404
aliforever <alipadida@live.com>
2023-01-07 22:06:45 +0300

    added settargeturl

--- proxy.go
+++ proxy.go
@@ -19,17 +19,19 @@ type Proxy struct {
 	httpUri string
 	mux     *http.ServeMux
 
-	homePageHandler    func(writer http.ResponseWriter, r *http.Request)
+	homePageHandler func(writer http.ResponseWriter, r *http.Request)
+
+	targetUrl          *url.URL
 	callbackUrlsLocker sync.Mutex
 	callbackUrls       map[string]*url.URL
 }
 
 func NewProxy(httpUri string) *Proxy {
-	return &Proxy{httpUri: httpUri, mux: http.NewServeMux()}
+	return &Proxy{httpUri: httpUri, mux: http.NewServeMux(), targetUrl: host}
 }
 
 func NewProxyWithMux(httpUri string, mux *http.ServeMux) *Proxy {
-	return &Proxy{httpUri: httpUri, mux: mux}
+	return &Proxy{httpUri: httpUri, mux: mux, targetUrl: host}
 }
 
 // EnableCallbackUrls by calling this method /add_callback_url endpoint will be activated
@@ -40,6 +42,13 @@ func (p *Proxy) EnableCallbackUrls() *Proxy {
 
 	p.callbackUrls = map[string]*url.URL{}
 	p.mux.HandleFunc("/add_callback_url", p.handleAddCallback)
+
+	return p
+}
+
+// SetTargetUrl is used to replace payment gateway's base url
+func (p *Proxy) SetTargetUrl(target *url.URL) *Proxy {
+	p.targetUrl = target
 
 	return p
 }
@@ -87,7 +96,7 @@ func (p *Proxy) handleRequests(writer http.ResponseWriter, request *http.Request
 
 	// If user has assigned a callback for this endpoint, forward the request there, else forward it to payment gateway
 	//	This is to proxy requests coming from payment gateway to custom urls
-	forwardTo := host
+	forwardTo := p.targetUrl
 	if callback := p.getEndpointCallback(request.URL.String()); callback != nil {
 		forwardTo = callback
 	}
